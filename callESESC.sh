d41500fa9047c341de6b3fdba76a2ac1d108464b
xzhan330 <57656169+xzhan330@users.noreply.github.com>
2019-11-14 23:02:00 -0800

    Update callESESC.sh

--- callESESC.sh
+++ callESESC.sh
@@ -5,8 +5,8 @@ export ESESC_ReportFile="callESESC"
 export ESESC_BenchName="./knightsTour.rv"
 
 export ESESC_samplerSel="TASS"
-export ESESC_TASS_nInstSkip="2e9"
-export ESESC_TASS_nInstMax="4e9"
+#export ESESC_TASS_nInstSkip="4e9"
+#export ESESC_TASS_nInstMax="8e9"
 
 if [ -f $ESESC_BIN ]; then
   $ESESC_BIN 
